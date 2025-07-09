23b00004c1b4e4a6564b05a5c70d519fa29452d3
Siddharth3710 <143256569+Siddharth3710@users.noreply.github.com>
2025-07-09 22:34:55 +0530

    Time: 9 ms (49.77%), Space: 42.8 MB (58.3%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,33 +1,41 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        List<List<String>> res = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        return backtrack(0, s1, s2, sb, new ArrayList<>(), res);
-    }
+        if (s1.length() > s2.length()) return false;
 
-    public boolean backtrack(int index, String s1, String s2, StringBuilder sb, List<String> path, List<List<String>> res) {
-        if (s1.length() == sb.length()) {
-            String perm = sb.toString();
-            if (s2.contains(perm)) {
-                return true;
-            }
-            return false;
-        }
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
 
         for (int i = 0; i < s1.length(); i++) {
-            if (sb.toString().indexOf(s1.charAt(i)) != -1) continue; // to avoid duplicates if needed
-
-            sb.append(s1.charAt(i));
-            path.add(String.valueOf(s1.charAt(i)));
-
-            if (backtrack(i + 1, s1, s2, sb, path, res)) {
-                return true;
-            }
-
-            sb.deleteCharAt(sb.length() - 1);
-            path.remove(path.size() - 1);
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
         }
 
-        return false;
+        int matches = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] == s2map[i]) matches++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches == 26) return true;
+
+            int r = s2.charAt(i + s1.length()) - 'a';
+            int l = s2.charAt(i) - 'a';
+
+            s2map[r]++;
+            if (s2map[r] == s1map[r]) {
+                matches++;
+            } else if (s2map[r] == s1map[r] + 1) {
+                matches--;
+            }
+
+            s2map[l]--;
+            if (s2map[l] == s1map[l]) {
+                matches++;
+            } else if (s2map[l] == s1map[l] - 1) {
+                matches--;
+            }
+        }
+
+        return matches == 26;
     }
 }
