51cb7ab9def7aa48537e329e39f3982f56894859
Siddharth3710 <143256569+Siddharth3710@users.noreply.github.com>
2025-07-12 00:23:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2105-watering-plants-ii/2105-watering-plants-ii.java
@@ -0,0 +1,49 @@
+public int minimumRefill(int[] arr, int capacityA, int capacityB) {
+
+    int cnt = 0; // number of refills
+    int remainingA = capacityA;
+    int remainingB = capacityB;
+    int n = arr.length;
+
+    if (n == 1) return 0;
+
+    int r = n - 1;
+
+    for (int l = 0; l < n; ++l) {
+
+        if (l == r) {
+            if (remainingA >= remainingB) {
+                if (remainingA < arr[l]) {
+                    remainingA = capacityA;
+                    cnt++;
+                }
+            } 
+            else {
+                if (remainingB < arr[r]) {
+                    remainingB = capacityB;
+                    cnt++;
+                }
+            }
+            break; 
+        }
+
+        if (remainingA < arr[l]) {
+            remainingA = capacityA;
+            cnt++;
+        }
+
+        if (remainingB < arr[r]) {
+            remainingB = capacityB;
+            cnt++;
+        }
+
+        remainingA -= arr[l];
+        remainingB -= arr[r];
+
+        r--;
+
+        if (l == r) break;
+    }
+
+    return cnt;
+}
