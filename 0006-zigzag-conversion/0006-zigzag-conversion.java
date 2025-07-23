5b561b105b611c18fd19afd56bc76e73ef9ef6aa
Siddharth3710 <143256569+Siddharth3710@users.noreply.github.com>
2025-07-23 23:14:21 +0530

    Time: 3 ms (90.87%), Space: 44.5 MB (97.47%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1){
+            return s;
+        }
+
+     StringBuilder sb = new StringBuilder();
+ 
+        for(int i=0;i<numRows;i++){
+            int idx=i;
+            int down=2*(numRows-i-1);
+            int diag=2*i;
+            boolean godown=true;
+
+            while(idx<s.length()){
+                sb.append(s.charAt(idx));
+
+
+                if(i==0){
+                    idx+=down;
+            
+                }
+                else if(i==numRows-1){
+                    idx+=diag;
+                }
+                else{
+                    if(godown){
+                        idx+=down;
+                    }
+                    else{
+                        idx+=diag;
+                    }
+
+                    godown=!godown;
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
